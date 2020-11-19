fe2313457bb304984192071e3ef228667052bfa9
Patrick Boettcher <p@yai.se>
2020-11-19 10:27:39 +0100

    tests: simplify and comment test-data for traj-test

--- tests/test_trajectory.py
+++ tests/test_trajectory.py
@@ -27,27 +27,36 @@ class TestTrajectory(unittest.TestCase):
             )
 
     @data(
-        (np.array([[1, 1, 1]]), [Cuboid(0, 0, 0, 2, 2, 2)], np.array([[0, 0]])), # simple, one dot trajectory, objet-list
-        (np.array([[1, 1, 1]]), Cuboid(0, 0, 0, 2, 2, 2), np.array([[0, 0]])),  # single object (listified)
+        # simple, one dot trajectory, object-list
+        ([[1, 1, 1]], [Cuboid(0, 0, 0, 2, 2, 2)], [[0, 0]]),
 
-        (np.array([[2, 2, 2]]), [Cuboid(0, 0, 0, 1, 1, 1)], np.array([])),  # all points are outside
+        # same single object (listified)
+        ([[1, 1, 1]], Cuboid(0, 0, 0, 2, 2, 2), [[0, 0]]),
 
-        (np.array([[2, 2, 2]]), [], np.array([])),  # no selection objects
+        # all points are outside
+        ([[2, 2, 2]], Cuboid(0, 0, 0, 1, 1, 1), []),
 
-        (np.array([[-1, -1, -1], [1, 1, 1]]), [Cuboid(0, 0, 0, 2, 2, 2)], np.array([[1, 1]])),
-        (np.array([[-1, -1, -1], [1, 1, 1]]), [Cuboid(0, 0, 0, 2, 2, 2)], np.array([[1, 1]])),
-        (np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]]), [Cuboid(0, 0, 0, 2, 2, 2)], np.array([[1, 2]])),
+        # no selection objects -> gives empty interval-liste
+        ([[2, 2, 2]], [], []),
 
-        (np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1], [-1, -1, -1], [0, 0, 0], [1, 1, 1]]),
-         [Cuboid(0, 0, 0, 2, 2, 2)], np.array([[1, 2], [4, 5]])),
+        # single point interval with multi-point trajectory
+        ([[-1, -1, -1], [1, 1, 1]], Cuboid(0, 0, 0, 2, 2, 2), [[1, 1]]),
 
-        (np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1], [-1, -1, -1], [0, 0, 0], [1, 1, 1]]),
-         [Cuboid(0, 0, 0, 2, 2, 2), Sphere(1.5, 1.5, 1.5 , 1)], np.array([[2, 2], [5, 5]])),  # overlapping sphere and cuboid, logical_and
+        # multi-point interval with multi-point trajectory
+        ([[-1, -1, -1], [0, 0, 0], [1, 1, 1]], Cuboid(0, 0, 0, 2, 2, 2), [[1, 2]]),
+
+        # trajectory leaving et re-entering the cuboid -> two intervals
+        ([[-1, -1, -1], [0, 0, 0], [1, 1, 1], [3, 3, 3], [1, 1, 1], [0, 0, 0]],
+         [Cuboid(0, 0, 0, 2, 2, 2)], [[1, 2], [4, 5]]),
+
+        # overlapping sphere and cuboid, logical_and between them
+        ([[-1, -1, -1], [0, 0, 0], [1, 1, 1], [-1, -1, -1], [0, 0, 0], [1, 1, 1]],
+         [Cuboid(0, 0, 0, 2, 2, 2), Sphere(1.5, 1.5, 1.5, 1)], [[2, 2], [5, 5]]),
     )
     @unpack
     def test_trajectory_intervals_with_primitive_objects(self, trajectory, objects, expected):
         np.testing.assert_array_equal(
             broni.intervals(
-                broni.Trajectory(trajectory, np.arange(0, len(trajectory)), coordinate_system="GSE"),
+                broni.Trajectory(np.array(trajectory), np.arange(0, len(trajectory)), coordinate_system="GSE"),
                 objects),
-            expected)
+            np.array(expected))
