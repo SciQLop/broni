4fa49902307f966ffb04022e426ea8ee08534e13
Patrick Boettcher <p@yai.se>
2020-11-20 10:12:21 +0100

    make data and time_index container private
    add getter to access data in order to
    allow the creation of sperical-data on-the-fly
    
    Reduce some boilerplate on tests.

--- tests/test_cuboid.py
+++ tests/test_cuboid.py
@@ -4,32 +4,36 @@ import unittest
 from ddt import ddt, data, unpack
 
 import numpy as np
+
 from broni.shapes.primitives import Cuboid
+from broni import Trajectory
 
 
 @ddt
 class TestCuboid(unittest.TestCase):
-    def test_invalid_ctor_args_raise_exceptions(self):
+    def test_invalid_ctor_args_cuboid_without_volume(self):
         with self.assertRaises(ValueError):
             assert Cuboid(0, 0, 0, 0, 0, 0)
 
     @data(
-        (np.array([[1, 1, 1]]), Cuboid(0, 0, 0, 10, 10, 10),
-         np.array([True])),  # one point
-        (np.array([[0, 0, 0]]), Cuboid(0, 0, 0, 10, 10, 10),
-         np.array([True])),  # edge point
-        (np.array([[-1, 0, 0]]), Cuboid(0, 0, 0, 10, 10, 10),
-         np.array([False])),  # outside
-        (np.array([[-1, 0, 0], [0, 0, 0], [1, 1, 1], [11, 11, 11]]), Cuboid(0, 0, 0, 10, 10, 10),
-         np.array([False, True, True, False], dtype=bool)),  # multiple points
-        (np.array([[-1, 0, 0]]), Cuboid(-10, -10, -10, 0, 0, 0),
-         np.array([True])),  # a cuboid in another quadrant
-        (np.array([]), Cuboid(0, 0, 0, 1, 1, 1),
-         np.array([])),  # empty trajectory
-        (np.array([[0, 0, 0], [3, 3, 3], [2.9, 2.9, 2.9], [10, 10, 10],
-                   [10.1, 10.1, 10.1]]), Cuboid(10, 10, 10, 3, 3, 3),
-         np.array([False, True, False, True, False], dtype=bool)),  # Cuboid points "inverted"
+        ([[1, 1, 1]], Cuboid(0, 0, 0, 10, 10, 10),
+         [True]),  # one point
+        ([[0, 0, 0]], Cuboid(0, 0, 0, 10, 10, 10),
+         [True]),  # edge point
+        ([[-1, 0, 0]], Cuboid(0, 0, 0, 10, 10, 10),
+         [False]),  # outside
+        ([[-1, 0, 0], [0, 0, 0], [1, 1, 1], [11, 11, 11]], Cuboid(0, 0, 0, 10, 10, 10),
+         [False, True, True, False]),  # multiple points
+        ([[-1, 0, 0]], Cuboid(-10, -10, -10, 0, 0, 0),
+         [True]),  # a cuboid in another quadrant
+        ([[0, 0, 0], [3, 3, 3], [2.9, 2.9, 2.9], [10, 10, 10],
+          [10.1, 10.1, 10.1]], Cuboid(10, 10, 10, 3, 3, 3),
+         [False, True, False, True, False]),  # Cuboid points "inverted"
     )
     @unpack
     def test_cuboid_intersections(self, trajectory, shape, expected):
-        np.testing.assert_array_equal(shape.intersect(trajectory), expected)
+        np.testing.assert_array_equal(
+            shape.intersect(Trajectory(np.array(trajectory),
+                                       np.arange(0, len(trajectory)),
+                                       'gse')),
+            np.array(expected))
--- tests/test_trajectory.py
+++ tests/test_trajectory.py
@@ -11,19 +11,36 @@ from broni.shapes.primitives import Cuboid, Sphere
 
 @ddt
 class TestTrajectory(unittest.TestCase):
-    def test_invalid_ctor_args_raise_exceptions(self):
+    def test_invalid_ctor_args_need_3_columns_are_needed(self):
         with self.assertRaises(ValueError):
             assert broni.Trajectory(
                 np.array([[0, 0], [0, 0]], dtype=float),
                 [2, 2],
-                coordinate_system="GSE"
+                coordinate_system="gse"
             )
 
+    def test_invalid_ctor_args_need_2_dim_array(self):
+        with self.assertRaises(ValueError):
+            assert broni.Trajectory(
+                np.array([0, 0, 0], dtype=float),
+                [2, 2, 2],
+                coordinate_system="gse"
+            )
+
+    def test_invalid_ctor_args_empty_trajectory_not_supported(self):
+        with self.assertRaises(ValueError):
+            assert broni.Trajectory(
+                np.array([[]], dtype=float),
+                [],
+                coordinate_system="gse"
+            )
+
+    def test_invalid_ctor_args_time_series_has_to_match_trajectory_length(self):
         with self.assertRaises(ValueError):
             assert broni.Trajectory(
                 np.array([[0, 0, 0], [0, 0, 0]], dtype=float),
                 [1, 2, 3],
-                coordinate_system="GSE"
+                coordinate_system="gse"
             )
 
     @data(
@@ -36,7 +53,7 @@ class TestTrajectory(unittest.TestCase):
         # all points are outside
         ([[2, 2, 2]], Cuboid(0, 0, 0, 1, 1, 1), []),
 
-        # no selection objects -> gives empty interval-liste
+        # no selection objects -> gives empty interval-list
         ([[2, 2, 2]], [], []),
 
         # single point interval with multi-point trajectory
@@ -54,9 +71,26 @@ class TestTrajectory(unittest.TestCase):
          [Cuboid(0, 0, 0, 2, 2, 2), Sphere(1.5, 1.5, 1.5, 1)], [[2, 2], [5, 5]]),
     )
     @unpack
-    def test_trajectory_intervals_with_primitive_objects(self, trajectory, objects, expected):
+    def test_trajectory_intervals_with_primitive_objects_no_time_index(self, trajectory, objects, expected):
         np.testing.assert_array_equal(
             broni.intervals(
-                broni.Trajectory(np.array(trajectory), np.arange(0, len(trajectory)), coordinate_system="GSE"),
+                broni.Trajectory(np.array(trajectory),
+                                 np.arange(0, len(trajectory)),
+                                 'gse'),
+                objects),
+            np.array(expected))
+
+    @data(
+        # multi-point interval with multi-point trajectory
+        ([[-1, -1, -1], [0, 0, 0], [1, 1, 1]],
+         [1000, 1001, 1002],
+         Cuboid(0, 0, 0, 2, 2, 2),
+         [[1001, 1002]])
+    )
+    @unpack
+    def test_trajectory_intervals_with_dedicated_time_index(self, trajectory, time, objects, expected):
+        np.testing.assert_array_equal(
+            broni.intervals(
+                broni.Trajectory(np.array(trajectory), time, 'gse'),
                 objects),
             np.array(expected))
